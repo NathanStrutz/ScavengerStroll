148c8e5a7496a4199d2ba69503bdc0e8ddb7af92
Nathan Strutz <strutz@gmail.com>
2020-10-04 17:27:42 -0400

    Pretter formatting

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ new Vue({
   data: {
     items: {
       park: [
-        { name: "Gazebo", description: "A big white gazebo" },
-        { name: "Bird", description: "A bird" },
-        { name: "Tree", description: "A big tree" },
-        { name: "Grass", description: "A big patch of grass" },
+        { completed: false, name: "Gazebo", description: "A big white gazebo" },
+        { completed: false, name: "Bird", description: "A bird" },
+        { completed: false, name: "Tree", description: "A big tree" },
+        { completed: false, name: "Grass", description: "A big patch of grass" },
       ],
       trail: [
-        { name: "Bird", description: "A bird" },
-        { name: "Tree", description: "A big tree" },
+        { completed: false, name: "Bird", description: "A bird" },
+        { completed: false, name: "Tree", description: "A big tree" },
       ],
     },
   },
