c0017a99af57b438fd02d5ea38f004009c9e1c78
Nathan Strutz <strutz@gmail.com>
2020-10-04 17:19:36 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: "#app",
+  data: {
+    items: {
+      park: [
+        { name: "Gazebo", description: "A big white gazebo" },
+        { name: "Bird", description: "A bird" },
+        { name: "Tree", description: "A big tree" },
+        { name: "Grass", description: "A big patch of grass" },
+      ],
+      trail: [
+        { name: "Bird", description: "A bird" },
+        { name: "Tree", description: "A big tree" },
+      ],
+    },
+  },
+});
