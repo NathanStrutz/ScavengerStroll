9435a3443d16af1f1b78973f235ddef3db9bd74a
Nathan Strutz <strutz@gmail.com>
2020-10-18 23:02:53 -0400

    Apply new image setup.

--- Readme.md
+++ Readme.md
@@ -7,13 +7,11 @@
 
 ## TO DO:
 
-- Add a page with links to nature information
-- Come up with text for the home page - explain what this is, why we made it, how it works, and what to do
-- Home page needs a couple big buttons to choose betwen nature and park
-- Header should be responsive, header images should respond to browser size
 - Click an image to zoom in
 - Create thumbnails for images
 - Vertically center checkboxes and text
+- Detach the big links on the home page
+- Fireworks - celebrate!
 
 ### Then...
 
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const ScavengerItem = Vue.component("ScavengerItem", {
   template: `
     <div :class="classes" @click="select(item)">
-      <img v-if="item.image" :src="'images/'+item.image" @click="imageClick" />
+      <a :href="'images/fullsize/'+item.image"><img v-if="item.image" :src="'images/thumbnails/'+item.image" @click="imageClick" /></a>
       <span v-else class="no-image"></span>
       <input type="checkbox" v-model="item.completed" :class="{completed: item.completed}" />
       <span :class="{completed: item.completed}">{{ item.name }}</span>
@@ -12,12 +12,13 @@ const ScavengerItem = Vue.component("ScavengerItem", {
   },
   computed: {
     classes() {
-      return `list-group-item item ${this.item.completed ? "completed" : "incomplete"}`;
+      return `list-group-item item ${this.item.completed ? "complete" : "incomplete"}`;
     },
   },
   methods: {
     select() {
       this.item.completed = !this.item.completed;
+      this.$emit("selected", this.item);
     },
     /**
      * Image click event handler
@@ -33,18 +34,34 @@ const ScavengerItemsMixin = {
   data: function () {
     return {
       items: [],
-      numberToShow: 9,
+      numberToShow: 10,
     };
   },
   computed: {
     randomItems() {
       let randomItems = [];
-      for (let i = 0; i <= this.numberToShow; i++) {
+      for (let i = 0; i < this.numberToShow; i++) {
         let itemIndex = Math.floor(Math.random() * this.items.length);
         let item = this.items.splice(itemIndex, 1);
         randomItems.push(item[0]);
       }
       return randomItems;
+    },
+    isFinished() {
+      console.log("Is Finished?", this.randomItems.filter((it) => !it.completed).length, "/", this.randomItems.length);
+      return this.randomItems.filter((it) => !it.completed).length === 0;
+    },
+  },
+  methods: {
+    getIsFinished() {
+      console.log("Is Finished?", this.randomItems.filter((it) => !it.completed).length, "/", this.randomItems.length);
+      return this.randomItems.filter((it) => !it.completed).length === 0;
+    },
+    selectedItem(item) {
+      if (this.getIsFinished()) {
+        // window.startFireworks();
+        console.log("Starting Fireworks!");
+      }
     },
   },
 };
@@ -52,7 +69,7 @@ const ScavengerItemsMixin = {
 const ParkScavengerItems = Vue.component("ParkScavengerItems", {
   template: `
     <div>
-      <scavenger-item v-for="item in randomItems" :item="item" :key="item.name" />
+      <scavenger-item v-for="item in randomItems" :item="item" :key="item.name" @selected="selectedItem" />
     </div>
     `,
   mixins: [ScavengerItemsMixin],
@@ -60,27 +77,32 @@ const ParkScavengerItems = Vue.component("ParkScavengerItems", {
   data: function () {
     return {
       items: [
-        { name: "Bikes", image: "IMG_5618.JPG", completed: false },
-        { name: "Playground", image: "IMG_5628.JPG", completed: false },
-        { name: "Trash can", image: "IMG_1618.JPEG", completed: false },
-        { name: "Wanamaker Oak", image: "IMG_5442.JPG", completed: false },
-        { name: "Park Center sign ", image: "IMG_5619.JPG", completed: false },
-        { name: "Vollyball net", image: "IMG_5603.JPG", completed: false },
-        { name: "Sprinkler", image: "IMG_1528.JPG", completed: false },
-        { name: "Map sign", image: "IMG_5544.JPG", completed: false },
-        { name: "Grill", image: "IMG_1530.JPEG", completed: false },
-        { name: "Magnolia shelter", image: "IMG_1541.JPEG", completed: false },
-        { name: "Picnic table", image: "IMG_1531.JPEG", completed: false },
-        { name: "Boat", image: "IMG_5445.JPG", completed: false },
-        { name: "Bench", image: "IMG_5566.JPG", completed: false },
-        { name: "Dimond trail sign (any color)", image: "IMG_5559.JPG", completed: false },
-        { name: "Pavillon", image: "IMG_5606.JPG", completed: false },
-        { name: "Restrooms", image: "IMG_5613.JPG", completed: false },
-        { name: "Big hill", image: "IMG_5630.JPG", completed: false },
-        { name: "Cypress Hall", image: "IMG_5597.JPG", completed: false },
-        { name: "Bench swing", image: "IMG_5617.JPG", completed: false },
+        { name: "Bikes", image: "IMG_5618.jpg", completed: false },
+        { name: "Playground", image: "IMG_5628.jpg", completed: false },
+        { name: "Trash can", image: "IMG_1618.jpg", completed: false },
+        { name: "Wanamaker Oak", image: "IMG_5442.jpg", completed: false },
+        { name: "Park Center sign ", image: "IMG_5619.jpg", completed: false },
+        { name: "Vollyball net", image: "IMG_5603.jpg", completed: false },
+        { name: "Sprinkler", image: "IMG_1528.jpg", completed: false },
+        { name: "Map sign", image: "IMG_5544.jpg", completed: false },
+        { name: "Grill", image: "IMG_1530.jpg", completed: false },
+        { name: "Magnolia shelter", image: "IMG_1541.jpg", completed: false },
+        { name: "Picnic table", image: "IMG_1531.jpg", completed: false },
+        { name: "Boat", image: "IMG_5445.jpg", completed: false },
+        { name: "Bench", image: "IMG_5566.jpg", completed: false },
+        { name: "Dimond trail sign (any color)", image: "IMG_5559.jpg", completed: false },
+        { name: "Pavillon", image: "IMG_5606.jpg", completed: false },
+        { name: "Restrooms", image: "IMG_5613.jpg", completed: false },
+        { name: "Big hill", image: "IMG_5630.jpg", completed: false },
+        { name: "Cypress Hall", image: "IMG_5597.jpg", completed: false },
+        { name: "Bench swing", image: "IMG_5617.jpg", completed: false },
       ],
     };
+  },
+  methods: {
+    selectedItem(item) {
+      console.log("nomix: Is Finished?", this.randomItems.filter((it) => !it.completed).length, "/", this.randomItems.length);
+    },
   },
 });
 
@@ -95,24 +117,24 @@ const NatureScavengerItems = Vue.component("NatureScavengerItems", {
   data: function () {
     return {
       items: [
-        { name: "Bird (cardinal, falcon, etc.)", image: "IMG_1621.JPG", completed: false },
-        { name: "Fallen tree", image: "IMG_5535.JPG", completed: false },
-        { name: "Spider (banana spider, black widow, etc.)", image: "IMG_5632.JPG", completed: false },
-        { name: "Reptile (turtle, alligator, etc.)", image: "IMG_4896.JPG", completed: false },
-        { name: "Mammal (deer, raccoon, etc.)", image: "IMG_1536.JPG", completed: false },
-        { name: "Creek", image: "IMG_5501.JPG", completed: false },
-        { name: "Hole in a tree", image: "IMG_5553.JPG", completed: false },
-        { name: "Split tree", image: "IMG_5538.JPG", completed: false },
-        { name: "Tree vine", image: "IMG_1597.JPEG", completed: false },
-        { name: "Spanish moss", image: "IMG_5503.JPG", completed: false },
-        { name: "Bird's nest", image: "IMG_1557.JPG", completed: false },
-        { name: "Insect (dragonfly, mosquito, etc.)", image: "IMG_5438.JPG", completed: false },
-        { name: "Algea", image: "IMG_5006.JPG", completed: false },
-        { name: "Tree", image: "IMG_5591.JPG", completed: false },
-        { name: "Rock", image: "IMG_5578.JPG", completed: false },
-        { name: "Cyprus knees", image: "IMG_5571.JPG", completed: false },
-        { name: "Tree stump", image: "IMG_5574.JPG", completed: false },
-        { name: "Lake", image: "IMG_5615.JPG", completed: false },
+        { name: "Bird (cardinal, falcon, etc.)", image: "IMG_1621.jpg", completed: false },
+        { name: "Fallen tree", image: "IMG_5535.jpg", completed: false },
+        { name: "Spider (banana spider, black widow, etc.)", image: "IMG_5632.jpg", completed: false },
+        { name: "Reptile (turtle, alligator, etc.)", image: "IMG_4896.jpg", completed: false },
+        { name: "Mammal (deer, raccoon, etc.)", image: "IMG_1536.jpg", completed: false },
+        { name: "Creek", image: "IMG_5501.jpg", completed: false },
+        { name: "Hole in a tree", image: "IMG_5553.jpg", completed: false },
+        { name: "Split tree", image: "IMG_5538.jpg", completed: false },
+        { name: "Tree vine", image: "IMG_1597.jpg", completed: false },
+        { name: "Spanish moss", image: "IMG_5503.jpg", completed: false },
+        { name: "Bird's nest", image: "IMG_1557.jpg", completed: false },
+        { name: "Insect (dragonfly, mosquito, etc.)", image: "IMG_5438.jpg", completed: false },
+        { name: "Algea", image: "IMG_5006.jpg", completed: false },
+        { name: "Tree", image: "IMG_5591.jpg", completed: false },
+        { name: "Rock", image: "IMG_5578.jpg", completed: false },
+        { name: "Cyprus knees", image: "IMG_5571.jpg", completed: false },
+        { name: "Tree stump", image: "IMG_5574.jpg", completed: false },
+        { name: "Lake", image: "IMG_5615.jpg", completed: false },
       ],
     };
   },
