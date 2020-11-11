3795ce867d0aeb34745e0df0e05b689b50a46eae
Nathan Strutz <strutz@gmail.com>
2020-11-11 08:12:36 -0500

    Experiment to fix zooming on iOS Safari

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const ScavengerItem = Vue.component("ScavengerItem", {
   template: `
     <div :class="itemClasses" @click="select">
-      <a class="zoom" v-if="item.image" :href="'images/fullsize/'+item.image"><img :src="'images/thumbnails/'+item.image" @click="imageClick" /></a>
+      <img v-if="item.image" :src="'images/thumbnails/'+item.image" :data-original="'images/fullsize/'+item.image" class="zoom" @click="imageClick" />
       <span v-else class="no-image"></span>
       <input type="checkbox" :checked="item.completed" :class="{complete: item.completed}" />
       <span :class="{complete: item.completed}">{{ item.name }}</span>
@@ -173,5 +173,5 @@ new Vue({
 // Image zoom via Zooming - https://github.com/kingdido999/zooming/
 new Zooming({
   bgColor: "rgba(0, 0, 0, 0.85)",
-  transitionDuration: 0.3
-}).listen("a.zoom img")
+  transitionDuration: 0.3,
+}).listen("img.zoom");
